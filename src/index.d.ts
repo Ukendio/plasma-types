e9c668535bc18b406a9e16f17e8e73ebe3524c6e
Marcus <ukendio@gmail.com>
2022-06-28 23:30:29 +0200

    export as Runtime

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,6 @@
 import { automaticSize } from "./automaticSize";
 import { create } from "./create";
-import { Node, scope, start, useEffect, useInstance, useState, widget } from "./Runtime";
+import { Node, Runtime } from "./Runtime";
 import { get, set } from "./Style";
 import { arrow } from "./widgets/arrow";
 import { blur } from "./widgets/blur";
@@ -10,27 +10,21 @@ import { portal } from "./widgets/portal";
 import { spinner } from "./widgets/spinner";
 import { window } from "./widgets/window";
 
-declare const Plasma: (new (rootInstance: Instance) => Node) & {
-	start: typeof start;
-	scope: typeof scope;
-	widget: typeof widget;
-	useState: typeof useState;
-	useInstance: typeof useInstance;
-	useEffect: typeof useEffect;
+declare const Plasma: (new (rootInstance: Instance) => Node) &
+	Runtime & {
+		useStyle: typeof get;
+		setStyle: typeof set;
 
-	useStyle: typeof get;
-	setStyle: typeof set;
+		automaticSize: typeof automaticSize;
+		create: typeof create;
 
-	automaticSize: typeof automaticSize;
-	create: typeof create;
-
-	window: typeof window;
-	button: typeof button;
-	portal: typeof portal;
-	blur: typeof blur;
-	spinner: typeof spinner;
-	checkbox: typeof checkbox;
-	arrow: typeof arrow;
-};
+		window: typeof window;
+		button: typeof button;
+		portal: typeof portal;
+		blur: typeof blur;
+		spinner: typeof spinner;
+		checkbox: typeof checkbox;
+		arrow: typeof arrow;
+	};
 
 export = Plasma;
