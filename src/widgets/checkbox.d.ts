47314857c5c70fcd3cb2e99f817ca1a9ff6855b0
Marcus <ukendio@gmail.com>
2022-06-29 01:11:11 +0200

    checkbox has clicked method

--- src/widgets/checkbox.d.ts
+++ src/widgets/checkbox.d.ts
@@ -1,5 +1,6 @@
 interface CheckboxWidgetHandle {
 	checked(): boolean;
+	clicked(): boolean;
 }
 
 export function checkbox(label: string, options?: { checked?: boolean; disabled?: boolean }): CheckboxWidgetHandle;
