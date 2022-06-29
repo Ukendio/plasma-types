0fd7b554295832086da32557b01036daa91a1eec
Marcus <ukendio@gmail.com>
2022-06-29 03:03:44 +0200

    union widgets

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,18 +2,7 @@ import { automaticSize } from "./automaticSize";
 import { create } from "./create";
 import { Node, Runtime } from "./Runtime";
 import { get, set } from "./Style";
-import { arrow } from "./widgets/arrow";
-import { blur } from "./widgets/blur";
-import { button } from "./widgets/button";
-import { checkbox } from "./widgets/checkbox";
-import { heading } from "./widgets/heading";
-import { label } from "./widgets/label";
-import { portal } from "./widgets/portal";
-import { row } from "./widgets/row";
-import { slider } from "./widgets/slider";
-import { space } from "./widgets/space";
-import { spinner } from "./widgets/spinner";
-import { window } from "./widgets/window";
+import { Widgets } from "./widgets";
 
 declare const Plasma: (new (rootInstance: Instance) => Node) &
 	Runtime & {
@@ -22,19 +11,6 @@ declare const Plasma: (new (rootInstance: Instance) => Node) &
 
 		automaticSize: typeof automaticSize;
 		create: typeof create;
-
-		window: typeof window;
-		button: typeof button;
-		portal: typeof portal;
-		blur: typeof blur;
-		row: typeof row;
-		spinner: typeof spinner;
-		checkbox: typeof checkbox;
-		arrow: typeof arrow;
-		heading: typeof heading;
-		label: typeof label;
-		slider: typeof slider;
-		space: typeof space;
-	};
+	} & Widgets;
 
 export = Plasma;
